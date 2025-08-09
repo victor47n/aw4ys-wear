5ea0525f33c7419d81802d3c0ce9f78ba1ed1321
victor47n <victor47n@gmail.com>
2025-08-09 08:58:35 -0300

    feat: ensure product can be removed from cart

--- src/_components/common/cart-item.tsx
+++ src/_components/common/cart-item.tsx
@@ -1,6 +1,9 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { MinusIcon, PlusIcon, TrashIcon } from "lucide-react";
 import Image from "next/image";
+import { toast } from "sonner";
 
+import { removeProductFromCart } from "@/_actions/remove-cart-product";
 import { formatCentsToBRL } from "@/_helpers/money";
 
 import { Button } from "../ui/button";
@@ -22,6 +25,26 @@ export const CartItem = ({
   productVariantPriceInCents,
   quantity,
 }: CartItemProps) => {
+  const queryClient = useQueryClient();
+  const removeProductFromCartMutation = useMutation({
+    mutationKey: ["remove-cart-product"],
+    mutationFn: () => removeProductFromCart({ cartItemId: id }),
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["cart"] });
+    },
+  });
+
+  const handleRemoveProduct = () => {
+    removeProductFromCartMutation.mutate(undefined, {
+      onSuccess: () => {
+        toast.success("Produto removido do carrinho!");
+      },
+      onError: (error) => {
+        toast.error("Erro ao remover produto do carrinho.");
+      },
+    });
+  };
+
   return (
     <div className="flex items-center justify-between">
       <div className="flex items-center gap-4">
@@ -49,7 +72,7 @@ export const CartItem = ({
         </div>
       </div>
       <div className="flex flex-col items-end justify-center gap-2">
-        <Button variant="outline" size="icon">
+        <Button variant="outline" size="icon" onClick={handleRemoveProduct}>
           <TrashIcon />
         </Button>
         <p className="text-xs font-medium">
