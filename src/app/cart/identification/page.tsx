8d3aca974b5d17e1c9d968bad9519d7a552d6bee
victor47n <victor47n@gmail.com>
2025-08-11 10:03:34 -0300

    feat: add cart summary components

--- src/app/cart/identification/page.tsx
+++ src/app/cart/identification/page.tsx
@@ -1,10 +1,12 @@
 import { headers } from "next/headers";
 import { redirect } from "next/navigation";
 
+import { Footer } from "@/_components/common/footer";
 import { Header } from "@/_components/common/header";
 import { auth } from "@/_lib/auth";
 import { db } from "@/db";
 
+import CartSummary from "../_components/cart-summary";
 import { Addresses } from "./_components/addresses";
 
 const IdentificationPage = async () => {
@@ -39,16 +41,34 @@ const IdentificationPage = async () => {
     orderBy: (address, { desc }) => desc(address.createdAt),
   });
 
+  const cartTotalInCents = cart.items.reduce(
+    (acc, item) => acc + item.productVariant.priceInCents * item.quantity,
+    0,
+  );
+
   return (
-    <>
+    <div className="space-y-12">
       <Header />
       <div className="space-y-4 px-5">
         <Addresses
           shippingAddresses={shippingAddresses}
           defaultShippingAddressId={cart.shippingAddress?.id || null}
         />
+        <CartSummary
+          subtotalInCents={cartTotalInCents}
+          totalInCents={cartTotalInCents}
+          products={cart.items.map((item) => ({
+            id: item.productVariant.id,
+            name: item.productVariant.product.name,
+            variantName: item.productVariant.name,
+            quantity: item.quantity,
+            priceInCents: item.productVariant.priceInCents,
+            imageUrl: item.productVariant.imageUrl,
+          }))}
+        />
       </div>
-    </>
+      <Footer />
+    </div>
   );
 };
 
